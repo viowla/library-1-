122c2b0037b531781b7adc70e26186e5060768ba
viowla <55015538+viowla@users.noreply.github.com>
2020-04-10 16:15:05 +0600

    commiting files

--- src/main/java/kz/iitu/library/controllers/BookController.java
+++ src/main/java/kz/iitu/library/controllers/BookController.java
@@ -1,4 +1,9 @@
 package kz.iitu.library.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
 }
--- src/main/java/kz/iitu/library/controllers/UserController.java
+++ src/main/java/kz/iitu/library/controllers/UserController.java
@@ -1,4 +1,22 @@
 package kz.iitu.library.controllers;
 
+import kz.iitu.library.classes.User;
+import kz.iitu.library.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("")
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
 }
