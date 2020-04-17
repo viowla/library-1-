3b72024e176467a69f9cef21ee4e663bea2778d0
viowla <55015538+viowla@users.noreply.github.com>
2020-04-17 15:05:50 +0600

    commiting files

--- src/main/java/kz/iitu/library/controllers/BookController.java
+++ src/main/java/kz/iitu/library/controllers/BookController.java
@@ -1,9 +1,37 @@
 package kz.iitu.library.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import kz.iitu.library.classes.Book;
+import kz.iitu.library.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("")
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book getUserById(@PathVariable("id") Long id) {
+        return bookRepository.findById(id).get();
+    }
+
+    @PostMapping("")
+    public Book createUser(@RequestBody Book book) {
+        return bookRepository.saveAndFlush(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id,
+                           @RequestBody Book book) {
+        book.setId(id);
+        return bookRepository.saveAndFlush(book);
+    }
 }
