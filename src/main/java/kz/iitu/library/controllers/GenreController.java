3fccd40e06ba175b58a8643584d5da09bea71dfb
viowla <55015538+viowla@users.noreply.github.com>
2020-04-26 12:03:16 +0600

    commiting files

--- src/main/java/kz/iitu/library/controllers/GenreController.java
+++ src/main/java/kz/iitu/library/controllers/GenreController.java
@@ -1,4 +1,37 @@
 package kz.iitu.library.controllers;
 
+import kz.iitu.library.classes.Author;
+import kz.iitu.library.classes.Genre;
+import kz.iitu.library.repositories.AuthorRepository;
+import kz.iitu.library.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genres")
 public class GenreController {
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @GetMapping("")
+    public List<Genre> getAllGenres(){
+        return genreRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Genre getGenreById(@PathVariable("id") Long id) {
+        return genreRepository.findById(id).get();
+    }
+
+    @DeleteMapping("")
+    public void deleteAllGenres(){
+        genreRepository.deleteAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGenreById(@PathVariable("id") Long id){
+        genreRepository.deleteById(id);
+    }
 }
--- src/main/java/kz/iitu/library/repositories/GenreRepository.java
+++ src/main/java/kz/iitu/library/repositories/GenreRepository.java
@@ -3,10 +3,12 @@ package kz.iitu.library.repositories;
 import kz.iitu.library.classes.Genre;
 import kz.iitu.library.classes.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
     @Override
     public List<Genre> findAll();
